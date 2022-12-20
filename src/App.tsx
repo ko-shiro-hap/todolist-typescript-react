133df4efe3231bd5145b29b5fd363cf7d58fc6b4
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-20 22:00:49 +0900

    feature:inputのイベントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  type Todo = {
+    inputValue: string;
+    id: number; //keyを指定するため
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
   return (
     <div className="App">
       <div>
         <h2>Todoリスト with Typescript</h2>
-        <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText" />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className="inputText" />
           <input type="submit" value="作成" className="submitButton" />
         </form>
       </div>
